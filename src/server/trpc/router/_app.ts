174356139ea978345dc0cfd8152d7788acf66f98
Bruno Mariano <bsignx@gmail.com>
2022-10-22 16:02:10 -0300

    refactor(trpc): remove example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 // src/server/router/_app.ts
 import { router } from '../trpc';
 
-import { exampleRouter } from './example';
 import { kanbanRouter } from './kanban';
 
 export const appRouter = router({
-  example: exampleRouter,
   kanban: kanbanRouter,
 });
 
