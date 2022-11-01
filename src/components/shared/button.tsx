a41e0b5c9d4026dc1dc092bb4b17a7716b5c1086
Bruno Mariano <bsignx@gmail.com>
2022-11-01 08:54:38 -0300

    refactor(button): add variants

--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -1,16 +1,45 @@
-import { Button as ChakraButton, ChakraProps } from '@chakra-ui/react';
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps,
+} from '@chakra-ui/react';
 import { ReactNode } from 'react';
+
+const variantsStyles = {
+  primary: {
+    color: 'light.100',
+    bgColor: 'primary',
+    _hover: {
+      bgColor: 'secondary',
+    },
+    _active: {
+      bgColor: 'secondary',
+    },
+  },
+  secondary: {
+    color: 'primary',
+    bgColor: 'light.100',
+    _hover: {
+      bgColor: 'secondary',
+      color: 'light.100',
+    },
+    _active: {
+      bgColor: 'secondary',
+    },
+  },
+};
 
 type ButtonProps = {
   children: ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
   disabled?: boolean;
-} & ChakraProps;
+  variant?: 'primary' | 'secondary';
+} & Omit<ChakraButtonProps, 'children' | 'onClick' | 'disabled' | 'variant'>;
 
 export const Button = ({
   onClick,
   children,
   disabled,
+  variant = 'primary',
   ...delegaded
 }: ButtonProps) => {
   return (
@@ -19,15 +48,8 @@ export const Button = ({
       disabled={disabled}
       fontWeight="700"
       fontSize="sm"
-      color="light.100"
-      bgColor="primary"
       borderRadius="24px"
-      _hover={{
-        bgColor: 'secondary',
-      }}
-      _active={{
-        bgColor: 'secondary',
-      }}
+      {...variantsStyles[variant]}
       {...delegaded}
     >
       {children}
