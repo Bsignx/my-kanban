157df6b2adbcaed3f1ae0acb6df8fcc0c06ab447
Bruno Mariano <bsignx@gmail.com>
2022-11-01 08:52:57 -0300

    feat: add close icon

---
+++ src/components/shared/svgs/close.tsx
@@ -0,0 +1,27 @@
+import { createIcon } from '@chakra-ui/react';
+
+export const Close = createIcon({
+  displayName: 'Close',
+  viewBox: '0 0 15 15',
+  path: (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="12.7275"
+        width="3"
+        height="18"
+        transform="rotate(45 12.7275 0)"
+      />
+      <rect
+        y="2.12134"
+        width="3"
+        height="18"
+        transform="rotate(-45 0 2.12134)"
+      />
+    </svg>
+  ),
+});
