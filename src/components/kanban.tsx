81505925943692e299dd1b618245308b95ae9ed2
Bruno Mariano <bsignx@gmail.com>
2022-11-01 08:56:48 -0300

    refactor(kanban): componentize

--- src/components/kanban.tsx
+++ src/components/kanban.tsx
@@ -1,76 +1,37 @@
-import { Box, Flex, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Box, HStack } from '@chakra-ui/react';
 
 import { useKanban } from '../contexts/kanban-context';
-import { Button } from './shared/button';
-import { Text } from './shared/text';
-
-const mockedTasks = [
-  {
-    id: 1,
-    title: 'Build UI for onboarding flow',
-    subtasksDescription: '0 of 3 substasks',
-  },
-  {
-    id: 2,
-    title: 'Build UI for search',
-    subtasksDescription: '0 of 1 substasks',
-  },
-  {
-    id: 3,
-    title: 'Build settings UI',
-    subtasksDescription: '0 of 2 substasks',
-  },
-  {
-    id: 4,
-    title: 'QA and test all major user journeys',
-    subtasksDescription: '0 of 2 substasks',
-  },
-];
+import { Task } from '../types/domain';
+import { BoardColumn } from './board-column';
+import { VoidBoard } from './void-board';
 
 export const Kanban = () => {
   const { tasksByStatus } = useKanban();
-
+  console.log(tasksByStatus);
   const isFilled = Object.keys(tasksByStatus).length > 0;
 
-  return <>{isFilled ? <FilledKanban /> : <VoidKanban />}</>;
-};
-
-const FilledKanban = () => {
   return (
-    <Flex>
-      <Box>
-        <Text>TODO (4)</Text>
-        <VStack>
-          {mockedTasks.map(({ id, subtasksDescription, title }) => (
-            <Box key={id}>
-              <Text>{title}</Text>
-              <Text>{subtasksDescription}</Text>
-            </Box>
-          ))}
-        </VStack>
-      </Box>
-    </Flex>
+    <Box p="6">
+      {isFilled ? <FilledBoard tasksByStatus={tasksByStatus} /> : <VoidBoard />}
+    </Box>
   );
 };
 
-const VoidKanban = () => {
-  const kanbanBgColor = useColorModeValue('light.200', 'dark.300');
-
+const FilledBoard = ({
+  tasksByStatus,
+}: {
+  tasksByStatus: Record<string, Task[]>;
+}) => {
   return (
-    <Flex
-      h="100%"
-      bg={kanbanBgColor}
-      flexDir="column"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Text variant="h3" color="dark.10" mb="7">
-        This board is empty. Create a new column to get started.
-      </Text>
-
-      <Button onClick={() => alert('clicked')} maxW="160px">
-        + Add New Column
-      </Button>
-    </Flex>
+    <HStack spacing="6" overflow="auto" maxW="77vw" alignItems="start">
+      {Object.entries(tasksByStatus).map(([status, tasks], index) => (
+        <BoardColumn
+          key={`${status}-${index}`}
+          index={index}
+          boardTitle={status}
+          tasks={tasks}
+        />
+      ))}
+    </HStack>
   );
 };
