4172759c0a83fafd6a13471c918830a0bb56ed38
Bruno Mariano <bsignx@gmail.com>
2022-11-07 06:31:00 -0300

    chore: setup ReactQueryDevtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppType } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@fontsource/plus-jakarta-sans/400.css';
 import '@fontsource/plus-jakarta-sans/500.css';
 import '@fontsource/plus-jakarta-sans/700.css';
@@ -11,6 +12,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <ReactQueryDevtools />
     </ChakraProvider>
   );
 };
